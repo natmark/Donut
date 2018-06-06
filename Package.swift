c392cd9e447caedec94c094f7a8d7ea2277d4db0
natmark <natmark0918@gmail.com>
2018-06-06 18:12:40 +0900

    :ok: Linking dependencies

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Donut",
-            dependencies: [],
+            dependencies: ["ReactiveTask", "Commandant"]),
             path: "Sources"),
         .testTarget(
             name: "DonutTests",
