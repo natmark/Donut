abf70e654ee4c4888f3d97c2bbc1c07f0dd15f33
natmark <natmark0918@gmail.com>
2018-06-07 17:34:01 +0900

    :ok: Fix SemanticVersion struct

--- Sources/DonutKit/SemanticVersion.swift
+++ Sources/DonutKit/SemanticVersion.swift
@@ -7,38 +7,18 @@
 
 import Foundation
 
-public protocol VersionType: Hashable {}
-
-/// A semantic version.
-public struct SemanticVersion: VersionType {
-    /// The major version.
-    ///
-    /// Increments to this component represent incompatible API changes.
+public struct SemanticVersion {
     public let major: Int
-
-    /// The minor version.
-    ///
-    /// Increments to this component represent backwards-compatible
-    /// enhancements.
     public let minor: Int
-
-    /// The patch version.
-    ///
-    /// Increments to this component represent backwards-compatible bug fixes.
     public let patch: Int
-
-    /// A list of the version components, in order from most significant to
-    /// least significant.
-    public var components: [Int] {
-        return [ major, minor, patch ]
-    }
-
     public init(major: Int, minor: Int, patch: Int) {
         self.major = major
         self.minor = minor
         self.patch = patch
     }
+}
 
+extension SemanticVersion {
     public func toString() -> String {
         return [major, minor, patch].map { String($0) }.joined(separator: ".")
     }
